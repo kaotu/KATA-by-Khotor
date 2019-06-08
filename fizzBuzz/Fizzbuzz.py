34de61e27d7a2d599d895956d170361e366f9d4b
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-08 11:07:20 +0700

    Add test case: Day3🌚

--- fizzBuzz/Fizzbuzz.py
+++ fizzBuzz/Fizzbuzz.py
@@ -5,35 +5,43 @@ class FizzBuzzTest(unittest.TestCase):
     def test_input_3_should_return_fizz(self):
         expected = 'fizz'
         actual = fizzbuzz(3)
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_input_5_should_return_buzz(self):
         expected = 'buzz'
         actual = fizzbuzz(5)
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
-    def test_input_2_should_return_2(self):
-        expected = 2
-        actual = fizzbuzz(2)
-        self.assertEqual(actual, expected)
+    def test_input_7_shuold_return_7(self):
+        expected = 7
+        actual = fizzbuzz(7)
+        self.assertEqual(expected, actual)
 
-    def test_input_6_should_return_fizz(self):
-        expected = 'fizz'
-        actual = fizzbuzz(6)
-        self.assertEqual(actual, expected)
+    def test_input_15_should_return_fizzbuzz(self):
+        expected = 'fizzbuzz'
+        actual = fizzbuzz(15)
+        self.assertEqual(expected, actual)
 
-    def test_input_10_should_return_10(self):
-        expected = 'buzz'
-        actual = fizzbuzz(10)
-        self.assertEqual(actual, expected)
+    def test_input_30_should_return_fizzbuzz(self):
+        expected = 'fizzbuzz'
+        actual = fizzbuzz(30)
+        self.assertEqual(expected, actual)
+
+    def test_input_0_should_return_0(self):
+        expected = 0
+        actual = fizzbuzz(0)
+        self.assertEqual(expected, actual)
 
 
 def fizzbuzz(number):
-    if number % 3 == 0:
-        return 'fizz'
+    if number == 0:
+        return 0
+    elif number % 3 == 0 and number % 5 == 0:
+        return 'fizzbuzz'
     elif number % 5 == 0:
         return 'buzz'
+    elif number % 3 == 0:
+        return 'fizz'
     return number
-
 
 unittest.main()
