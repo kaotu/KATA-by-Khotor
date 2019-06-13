9855f7df77ef54630bacb567c6bca9d572951dad
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-13 10:37:17 +0700

    Change obj of lift to dict

--- TDD-Lift/lift.py
+++ TDD-Lift/lift.py
@@ -1,22 +1,37 @@
 import unittest
 
 
-#lift : [[1,2],[2,3],[3,2]]
+#lift : [[1,2],[2,3],[3,5]]
 
 class LiftTest(unittest.TestCase):
-    def test_input_1_should_return_lift1(self):
-        expected = 'lift1'
+    def test_input_1_should_return_lift_1(self):
+        expected = 'lift_1'
         actual = lift(1)
         self.assertEqual(actual, expected)
 
-    def test_input_2_should_return_lift3(self):
-        expected = 'lift3'
+    def test_input_2_should_return_lift_1(self):
+        expected = 'lift_1'
         actual = lift(2)
         self.assertEqual(actual, expected)
 
+    def test_input_3_should_return_lift_2(self):
+        expected = 'lift_2'
+        actual = lift(3)
+        self.assertEqual(actual, expected)
+
+
+
+lift_all = [
+    {'lift':'lift_1', 'floor': 2},
+    {'lift':'lift_2', 'floor': 3},
+    {'lift':'lift_3', 'floor': 5}
+]
+
 def lift(input):
     if input == 2:
-        return 'lift3'
-    return 'lift1'
+        return lift_all[0].get('lift')
+    elif input == 3:
+        return lift_all[1].get('lift')
+    return lift_all[0].get('lift')
 
 unittest.main()
