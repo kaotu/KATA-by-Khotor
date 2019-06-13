e861c511c54fe182c5f8c45a203fe2c723ab2da7
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-13 08:23:31 +0700

    Change condition of test: don't use position

--- TDD-Lift/lift.py
+++ TDD-Lift/lift.py
@@ -4,19 +4,19 @@ import unittest
 #lift : [[1,2],[2,3],[3,2]]
 
 class LiftTest(unittest.TestCase):
-    def test_input_1_and_position_1_should_return_lift1(self):
+    def test_input_1_should_return_lift1(self):
         expected = 'lift1'
-        actual = lift(1, 1)
+        actual = lift(1)
         self.assertEqual(actual, expected)
 
-    def test_input_1_and_position_3_sholud_return_lift3(self):
+    def test_input_2_should_return_lift3(self):
         expected = 'lift3'
-        actual = lift(1, 1)
+        actual = lift(2)
         self.assertEqual(actual, expected)
 
-def lift(input, position):
-    if input == 1 and position == 1:
-        return 'lift1'
-    return 'lift3'
+def lift(input):
+    if input == 2:
+        return 'lift3'
+    return 'lift1'
 
 unittest.main()
