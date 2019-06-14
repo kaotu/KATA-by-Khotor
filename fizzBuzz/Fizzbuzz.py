6e0c723cee95b2c42d535ac920681997a6e9802b
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-14 09:31:59 +0700

    Add global variable : :

--- fizzBuzz/Fizzbuzz.py
+++ fizzBuzz/Fizzbuzz.py
@@ -1,44 +1,54 @@
 import unittest
 
 
+fizz = 'fizz'
+buzz = 'buzz'
+fizzbuzz = 'fizzbuzz'
+
 class FizzBuzzTest(unittest.TestCase):
+
+    def test_input_3_sholud_return_fizz(self):
+        expected = fizz
+        actual = fizz_buzz(3)
+        self.assertEqual(actual, expected)
+
     def test_input_5_should_return_buzz(self):
-        expected = 'buzz'
+        expected = buzz
         actual = fizz_buzz(5)
         self.assertEqual(actual, expected)
 
-    def test_input_4_should_return_4(self):
-        expected = 4
-        actual = fizz_buzz(4)
+    def test_input_1_should_return_1(self):
+        expected = 1
+        actual = fizz_buzz(1)
+        self.assertEqual(actual, expected)
+
+    def test_input_6_should_return_fizz(self):
+        expected = fizz
+        actual = fizz_buzz(6)
+        self.assertEqual(actual, expected)
+
+    def test_input_10_should_return_buzz(self):
+        expected = buzz
+        actual = fizz_buzz(10)
         self.assertEqual(actual, expected)
 
     def test_input_15_should_return_fizzbuzz(self):
-        expected = 'fizzbuzz'
+        expected = fizzbuzz
         actual = fizz_buzz(15)
         self.assertEqual(actual, expected)
 
-    def test_input_7_should_return_whoof(self):
-        expected = 'whoof'
-        actual = fizz_buzz(7)
+    def test_input_30_should_return_fizzbuzz(self):
+        expected = fizzbuzz
+        actual = fizz_buzz(30)
         self.assertEqual(actual, expected)
 
-    def test_input_divided_by_3_should_return_fizz(self):
-        expected = 'fizz'
-        actual = fizz_buzz(3)
-        actual_2 = fizz_buzz(6)
-        actual_3 = fizz_buzz(9)
-
-        self.assertEqual(actual, expected)
-        self.assertEqual(actual_2, expected)
-        self.assertEqual(actual_3, expected)
 
 def fizz_buzz(number):
     result = {
-        True: number,
-        number % 3 == 0: 'fizz',
-        number % 5 == 0: 'buzz',
-        number % 15 == 0: 'fizzbuzz',
-        number == 7: 'whoof'
+        number: True,
+        number % 3 == 0: fizz,
+        number % 5 == 0: buzz,
+        number % 15 == 0: fizzbuzz
     }
     return result.get(True)
 
